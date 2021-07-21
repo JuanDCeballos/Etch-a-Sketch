e1658677942f3cd67722c59c469fc2cb4bdc33af
Juan Ceballos <juandavi789@hotmail.com>
2021-07-20 23:00:03 -0500

    add some functionality like for example some different colors and now i'm working on the resize button, hope to finish this project tomorrow

--- js/main.js
+++ js/main.js
@@ -1,22 +1,51 @@
-const container = document.querySelector('.container');
-const clearBtn = document.getElementById('clearBtn');
+const grid = document.querySelector('.grid');
+const changeSize = document.getElementById('changeSize');
+const eraser = document.getElementById('eraser');
+const rgbColor = document.getElementById('rgbColor');
+const grayColor = document.getElementById('gray');
 
-clearBtn.addEventListener('click', clearGrid);
+// let finalGrid = 2 * 2;
+// grid.style.gridTemplateColumns = `repeat(2, 1fr)`;
 
-let n = 0;
-let finalGrid = 0;
-container.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+// let div = undefined;
 
-for (let i = 0; i < finalGrid; i++) {
-  const div = document.createElement('div');
-  div.style.border = '1px solid gray';
-  container.appendChild(div);
-}
+// for (let i = 0; i < finalGrid; i++) {
+//   const div = document.createElement('div');
+//   // div = document.createElement('div');
+//   div.style.border = '1px solid black';
+//   grid.append(div);
+// }
 
-function clearGrid() {
-  n = prompt('Enter a number between 1 and 64', 16);
-  if (n < 1 || n != Number) {
-  } else {
-    finalGrid = n * n;
+grid.addEventListener('mouseover', (e) => {
+  e.target.style.backgroundColor = 'gray';
+});
+
+grayColor.addEventListener('click', () => {
+  grid.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = 'gray';
+  });
+});
+
+rgbColor.addEventListener('click', () => {
+  grid.addEventListener('mouseover', (e) => {
+    let r = () => (Math.random() * 256) >> 0;
+    e.target.style.backgroundColor = `rgb(${r()}, ${r()}, ${r()})`;
+  });
+});
+
+eraser.addEventListener('click', () => {
+  grid.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = '#f2f2f2';
+  });
+});
+
+changeSize.addEventListener('click', () => {
+  let numberOfGrids = prompt('Enter a number between 1 and 64', 16);
+  let finalNumberOfGrids = numberOfGrids * numberOfGrids;
+  for (let i = 0; i < finalNumberOfGrids; i++) {
+    const divGrid = document.createElement('div');
+    divGrid.style.border = '1px solid black';
+    grid.append(divGrid);
   }
-}
+  grid.style.gridTemplateColumns = `repeat(${numberOfGrids}, 1fr)`;
+});
